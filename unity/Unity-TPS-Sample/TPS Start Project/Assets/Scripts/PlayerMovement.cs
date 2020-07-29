06bd70816a35faa6f39e515fb7cd2feeea746692
eldertree777 <34835912+eldertree777@users.noreply.github.com>
2020-07-29 20:50:20 +0900

    20.07.29

--- unity/Unity-TPS-Sample/TPS Start Project/Assets/Scripts/PlayerMovement.cs
+++ unity/Unity-TPS-Sample/TPS Start Project/Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
     
     public float speed = 6f;
     public float jumpVelocity = 20f;
-    [Range(0.01f, 1f)] public float airControlPercent;
+    [Range(0.01f, 1f)] public float airControlPercent; //공중에 떠있는동안 이동조작
 
     public float speedSmoothTime = 0.1f;
     public float turnSmoothTime = 0.1f;
@@ -19,13 +19,17 @@ public class PlayerMovement : MonoBehaviour
     private float turnSmoothVelocity;
     
     private float currentVelocityY;
-    
+
     public float currentSpeed =>
         new Vector2(characterController.velocity.x, characterController.velocity.z).magnitude;
+        
     
     private void Start()
     {
-
+        playerInput = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+        followCam = Camera.main;
     }
 
     private void FixedUpdate()
@@ -39,26 +43,51 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        
+        UpdateAnimation(playerInput.moveInput);
     }
 
     public void Move(Vector2 moveInput)
     {
+        var targetSpeed = speed * moveInput.magnitude;
+        var moveDirection = Vector3.Normalize(transform.forward * moveInput.y + transform.right * moveInput.x);
 
+        var smoothTime = characterController.isGrounded ? speedSmoothTime: speedSmoothTime / airControlPercent;
+        //공중에서 컨트롤 느리게
+
+        targetSpeed = Mathf.SmoothDamp(currentSpeed , targetSpeed ,ref speedSmoothVelocity , smoothTime);
+
+        currentVelocityY += Time.deltaTime * Physics.gravity.y;
+        // 속도 = 시간분활 * 중력 가속도
+
+        var velocity = moveDirection * targetSpeed + Vector3.up * currentVelocityY;
+        // 속도는 = 방향 * 속도 + (0,1,0) * currentVelocityY
+
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (characterController.isGrounded) currentVelocityY = 0f;
     }
 
+    //캐릭터 이동시 캐릭터 회전
     public void Rotate()
     {
+        var targetRotation = followCam.transform.eulerAngles.y;
 
+        targetRotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+
+
+        transform.eulerAngles = Vector3.up * targetRotation;
     }
 
     public void Jump()
     {
-        
+        if (!characterController.isGrounded) return;
+        currentVelocityY = jumpVelocity;
     }
 
     private void UpdateAnimation(Vector2 moveInput)
     {
-
+        var animationSpeedPercent = currentSpeed / speed;
+        animator.SetFloat("Vertical Move", moveInput.y * animationSpeedPercent , 0.05f , Time.deltaTime);
+        animator.SetFloat("Horizontal Move", moveInput.x * animationSpeedPercent, 0.05f , Time.deltaTime);
     }
 }
