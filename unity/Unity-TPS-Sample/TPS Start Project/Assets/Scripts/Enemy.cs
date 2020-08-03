24649efcff8a5d73339d4167263837ccdf9f3be5
eldertree777 <34835912+eldertree777@users.noreply.github.com>
2020-08-03 19:25:26 +0900

    20.08.03

--- unity/Unity-TPS-Sample/TPS Start Project/Assets/Scripts/Enemy.cs
+++ unity/Unity-TPS-Sample/TPS Start Project/Assets/Scripts/Enemy.cs
@@ -40,7 +40,7 @@ public class Enemy : LivingEntity
     private float attackDistance;
     
     public float fieldOfView = 50f;
-    public float viewDistance = 10f;
+    public float viewDistance = 50f;
     public float patrolSpeed = 3f;
     
     public LivingEntity targetEntity;
@@ -112,29 +112,128 @@ public class Enemy : LivingEntity
 
     private void Update()
     {
+        if (dead)
+        {
+            return;
+        }
 
+        if(state == State.Tracking)
+        {
+            var distance = Vector3.Distance(targetEntity.transform.position, transform.position);
+
+            if(distance <= attackDistance)
+            {
+                BeginAttack();
+            }
+        }                           
     }
 
     private void FixedUpdate()
     {
         if (dead) return;
+
+
+        if(state == State.AttackBegin || state == State.Attacking)
+        {
+            var lookRotation = Quaternion.LookRotation(targetEntity.transform.position - transform.position);
+            var targetAngleY = lookRotation.eulerAngles.y;
+
+            targetAngleY = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngleY, ref turnSmoothVelocity, turnSmoothTime);
+            transform.eulerAngles = Vector3.up * targetAngleY;
+        } 
+
+        if(state == State.Attacking)
+        {
+            var direction = transform.forward;
+            var deltaDistance = agent.velocity.magnitude * Time.deltaTime;
+
+            var size = Physics.SphereCastNonAlloc(attackRoot.position, attackRadius, direction, hits, deltaDistance, whatIsTarget);
+
+            for(var i=0; i < size; i++)
+            {
+                var attackTargetEntity = hits[i].collider.GetComponent<LivingEntity>();
+                if(attackTargetEntity != null && !lastAttackedTargets.Contains(attackTargetEntity))
+                {
+                    var message = new DamageMessage();
+                    message.amount = damage;
+                    message.damager = gameObject;
+
+                    if(hits[i].distance <= 0f)
+                    {
+                        message.hitPoint = attackRoot.position;
+                    }
+                    else
+                    {
+                        message.hitPoint = hits[i].point;
+                    }
+
+                    message.hitNormal = hits[i].normal;
+
+                    attackTargetEntity.ApplyDamage(message);
+                    lastAttackedTargets.Add(attackTargetEntity);
+                    break;
+                }
+            }
+        }
     }
 
     private IEnumerator UpdatePath()
     {
         while (!dead)
         {
+            
             if (hasTarget)
             {
+                if(state == State.Patrol)
+                {
+                    state = State.Tracking;
+                    agent.speed = runSpeed;
+                }
                 agent.SetDestination(targetEntity.transform.position);
                 // 추적대상위치 
+                
             }
             else
             {
+                
                 if (targetEntity != null) targetEntity = null;
+
+                if(state != State.Patrol)
+                {
+                    state = State.Patrol;
+                    agent.speed = patrolSpeed;
+                }
+
+
+                if(agent.remainingDistance <= 1f)
+                {
+                    var partrolTargetPosition = Utility.GetRandomPointOnNavMesh(transform.position, 20f, NavMesh.AllAreas); // 랜덤위치
+                    agent.SetDestination(partrolTargetPosition);
+                }
+
+                var colliders = Physics.OverlapSphere(eyeTransform.position, viewDistance , whatIsTarget);
+
+                foreach(var collider in colliders)
+                {
+                    if (!IsTargetOnSight(collider.transform))
+                    {
+                       
+                        continue;
+                    }
+
+                    var livingEntity = collider.GetComponent<LivingEntity>();
+
+                    if(livingEntity != null )//&& !livingEntity.dead)
+                    {
+                        
+                        targetEntity = livingEntity;
+                        break;
+                    }
+                }
             }
-            
-            yield return new WaitForSeconds(0.2f);
+            //animator.SetFloat("Speed", agent.speed);
+            animator.SetFloat("Speed", agent.desiredVelocity.magnitude);
+            yield return new WaitForSeconds(0.05f);
         }
     }
     
@@ -142,6 +241,15 @@ public class Enemy : LivingEntity
     {
         if (!base.ApplyDamage(damageMessage)) return false;
         
+        if(targetEntity == null)
+        {
+            targetEntity = damageMessage.damager.GetComponent<LivingEntity>();
+        }
+
+        EffectManager.Instance.PlayHitEffect(damageMessage.hitPoint, damageMessage.hitNormal, transform, EffectManager.EffectType.Flesh);
+
+        audioPlayer.PlayOneShot(hitClip);
+
         return true;
     }
 
@@ -150,9 +258,12 @@ public class Enemy : LivingEntity
         state = State.AttackBegin;
 
         agent.isStopped = true;
+
         animator.SetTrigger("Attack");
+        animator.SetFloat("Speed", agent.desiredVelocity.magnitude);
     }
 
+    //
     public void EnableAttack()
     {
         state = State.Attacking;
@@ -162,18 +273,58 @@ public class Enemy : LivingEntity
 
     public void DisableAttack()
     {
-        state = State.Tracking;
+        if (hasTarget)
+        {
+
+            state = State.Tracking;
+        }
+        else
+        {
+            state = State.Patrol;
+        }
         
         agent.isStopped = false;
     }
+    //애니메이션을 통해 구현됨
+
 
     private bool IsTargetOnSight(Transform target)
     {
+
+        var direction = target.position - eyeTransform.position;
+        direction.y = eyeTransform.forward.y;
+
+        if(Vector3.Angle(direction, eyeTransform.forward) > fieldOfView * 0.5f)
+        {
+            return false;
+        }
+
+        direction = target.position - eyeTransform.position;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(eyeTransform.position, direction , out hit, viewDistance , whatIsTarget))
+        {
+            if(hit.transform == target)
+            {
+                return true;
+            }
+        }
+
         return false;
     }
     
     public override void Die()
     {
+        base.Die();
 
+        GetComponent<Collider>().enabled = false;
+        //agent.isStopped = true;
+        agent.enabled = false;
+
+        animator.applyRootMotion = true;
+        animator.SetTrigger("Die");
+
+        audioPlayer.PlayOneShot(deathClip);
     }
 }
